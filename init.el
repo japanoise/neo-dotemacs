d5ad143d04339cd9bbc7bf9349cf75ad388a5db5
japanoise <japanoise@seekrit.club>
2021-05-12 22:56:04 -0500

    Add ascii table command from emacswiki

--- init.el
+++ init.el
@@ -183,6 +183,30 @@
 ;; C - some setup
 (setq c-default-style "linux")
 
+;; Ascii table - https://www.emacswiki.org/emacs/AsciiTable
+(defun ascii-table ()
+  "Display basic ASCII table (0 thru 128)."
+  (interactive)
+  (switch-to-buffer "*ASCII*")
+  (erase-buffer)
+  (setq buffer-read-only nil)        ;; Not need to edit the content, just read mode (added)
+  (local-set-key "q" 'bury-buffer)   ;; Nice to have the option to bury the buffer (added)
+  (setq lower32 '("nul" "soh" "stx" "etx" "eot" "enq" "ack" "bel"
+  		  "bs" "ht" "nl" "vt" "np" "cr" "so" "si"
+  		  "dle" "dc1" "dc2" "dc3" "dc4" "nak" "syn" "etb"
+  		  "can" "em" "sub" "esc" "fs" "gs" "rs" "us"
+  		  ))
+  (save-excursion (let ((i -1))
+                    (insert "ASCII characters 0 thru 127.\n\n")
+                    (insert " Hex  Dec  Char|  Hex  Dec  Char|  Hex  Dec  Char|  Hex  Dec  Char\n")
+                    (while (< i 31)
+                      (insert (format "  %02x %4d %4s |   %02x %4d %4s |   %02x %4d %4s |   %02x %4d %4s\n"
+                                      (setq i (+ 1  i)) i (elt lower32 i)
+                                      (setq i (+ 32 i)) i (single-key-description i)
+                                      (setq i (+ 32 i)) i (single-key-description i)
+                                      (setq i (+ 32 i)) i (single-key-description i)))
+                      (setq i (- i 96))))))
+
 ;; --------------------------- PACKAGES ------------------------------
 
 (require 'package)
