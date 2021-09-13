b17c566dd24360976e2f8e3f6f020fddb37632c5
japanoise <japanoise@seekrit.club>
2021-09-12 20:56:42 -0500

    Add xah-new-empty-buffer

--- init.el
+++ init.el
@@ -40,6 +40,25 @@
   (global-set-key (kbd "<f5>")
                   'chameleon-prefix-map))
 
+;; New empty buffer
+(defun xah-new-empty-buffer ()
+  "Create a new empty buffer.
+New buffer will be named \"untitled\" or \"untitled<2>\", \"untitled<3>\", etc.
+
+It returns the buffer (for elisp programing).
+
+URL `http://ergoemacs.org/emacs/emacs_new_empty_buffer.html'
+Version 2017-11-01"
+  (interactive)
+  (let (($buf (generate-new-buffer "untitled")))
+    (switch-to-buffer $buf)
+    (funcall initial-major-mode)
+    (setq buffer-offer-save t)
+    (text-mode)
+    $buf))
+
+(global-set-key (kbd "<f7>") 'xah-new-empty-buffer)
+
 ;; Don't emulate xterm please
 (setq frame-resize-pixelwise t)
 
