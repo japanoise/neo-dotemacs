f43b494a4d09d62ddedc33875e6c1d52c8dbb459
japanoise <japanoise@seekrit.club>
2021-12-25 00:13:35 -0600

    Add word-wrap

--- init.el
+++ init.el
@@ -443,6 +443,11 @@ Version 2017-11-01"
 (doom-modeline-mode 1)
 (setq doom-modeline-continuous-word-count-modes '(markdown-mode gfm-mode org-mode))
 
+;; Word wrap
+(use-package visual-fill-column)
+(add-hook 'visual-line-mode-hook #'visual-fill-column-mode)
+(setq-default visual-fill-column-center-text t)
+
 ;; ----------------------------- END ---------------------------------
 
 ;; Snazzy theme!
@@ -469,7 +474,7 @@ Version 2017-11-01"
  '(doom-modeline-height 21)
  '(doom-modeline-mode t)
  '(package-selected-packages
-   '(go-rename company-go godoctor rainbow-mode exec-path-from-shell browse-kill-ring dumb-jump go-mode company auto-complete auto-package-update no-littering editorconfig smex markdown-mode wc-mode flycheck smartparens rainbow-delimiters delight base16-theme diminish anzu use-package)))
+   '(visual-fill-column go-rename company-go godoctor rainbow-mode exec-path-from-shell browse-kill-ring dumb-jump go-mode company auto-complete auto-package-update no-littering editorconfig smex markdown-mode wc-mode flycheck smartparens rainbow-delimiters delight base16-theme diminish anzu use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
