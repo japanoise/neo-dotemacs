d94ac19ea02f8ef04a6cabfab6dac8cfd0833242
japanoise <japanoise@seekrit.club>
2022-01-25 21:37:06 -0600

    Disable org-link-descriptive
    This means links will be displayed as what the underlying textual form
    is, rather than as a pretty, "clicky" link whose guts are opaque. I
    just prefer it this way (coming from the markdown world)

--- init.el
+++ init.el
@@ -477,6 +477,7 @@ Version 2017-11-01"
  '(doom-modeline-enable-word-count t)
  '(doom-modeline-height 21)
  '(doom-modeline-mode t)
+ '(org-link-descriptive nil)
  '(package-selected-packages
    '(webpaste visual-fill-column go-rename company-go godoctor rainbow-mode exec-path-from-shell browse-kill-ring dumb-jump go-mode company auto-complete auto-package-update no-littering editorconfig smex markdown-mode wc-mode flycheck smartparens rainbow-delimiters delight base16-theme diminish anzu use-package)))
 (custom-set-faces
