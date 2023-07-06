51d20795bd016f4292ec1ead0b5da877fff14997
japanoise <japanoise@seekrit.club>
2023-07-06 08:04:08 -0500

    Stop littering undo-tree files
    I don't even use the more complicated features of undo-tree. There's
    no way I want my filesystem littered with these turds.

--- init.el
+++ init.el
@@ -528,7 +528,8 @@ Version 2017-11-01"
  '(lua-indent-level 2)
  '(org-link-descriptive nil)
  '(package-selected-packages
-   '(renpy treemacs elixir-mode cmake-mode lua-mode lsp-mode elpher webpaste visual-fill-column go-rename company-go godoctor rainbow-mode exec-path-from-shell browse-kill-ring dumb-jump go-mode company auto-complete auto-package-update no-littering editorconfig smex markdown-mode wc-mode flycheck smartparens rainbow-delimiters delight base16-theme diminish anzu use-package)))
+   '(renpy treemacs elixir-mode cmake-mode lua-mode lsp-mode elpher webpaste visual-fill-column go-rename company-go godoctor rainbow-mode exec-path-from-shell browse-kill-ring dumb-jump go-mode company auto-complete auto-package-update no-littering editorconfig smex markdown-mode wc-mode flycheck smartparens rainbow-delimiters delight base16-theme diminish anzu use-package))
+ '(undo-tree-auto-save-history nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
